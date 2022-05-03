ff369706598e6c779898391f7a5429e5c086ff81
Pedro Cm <pedromooreira@gmail.com>
2022-05-03 18:55:06 -0300

    Subindo primeiras config

---
+++ paradigmas/src/main/java/com/ufrpe/paradigmas/conexoes/ConexaoSQLite.java
@@ -0,0 +1,25 @@
+package com.ufrpe.paradigmas.conexoes;
+
+import java.sql.Connection;
+
+public class ConexaoSQLite {
+	
+	private Connection connection;
+
+	
+	public boolean conectar() {
+		
+		try {
+			String url = 
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return true;
+		
+	}
+	
+	public boolean desconectar() {
+		return true;
+	}
+}
